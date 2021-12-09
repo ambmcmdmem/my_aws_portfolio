e31429411339ce5f17da8d8374cf1259c3b307cf
ambmcmdmem <ambmcmdmem@au.com>
2021-12-09 05:14:27 +0000

    メール機能作成など

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Production;
+
+class WelcomeController extends Controller
+{
+    //
+
+    public function index() {
+        $productions = Production::all();
+        // ログインしていたら
+        if (auth()->user() != null) {
+            $my_id = auth()->user()->id;
+            $productions = $productions->where('user_id', '!=', $my_id);
+        }
+
+        return view('welcome', compact('productions'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,14 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
+
+// Auth::routes();
+Auth::routes(['verify' => true]);
+
+Route::middleware(['verified'])->group(function(){
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
